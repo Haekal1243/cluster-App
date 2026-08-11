5ab1cae13bd46e1c3bcddea062ba3eda698ca135
haekal1243 <haekal.putraalharis2017@gmail.com>
2026-08-11 18:14:15 +0700

    Berhenti melacak folder .next yang selalu berubah

--- frontend/.next/dev/static/chunks/frontend_src_app_page_jsx_1_o-87f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_20ri6o0._.js"],
-    source: "dynamic"
-});
--- frontend/.next/server/chunks/ssr/frontend__next-internal_server_app_register_page_actions_0o93wtk.js
+++
@@ -1,3 +0,0 @@
-module.exports=[236,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=frontend__next-internal_server_app_register_page_actions_0o93wtk.js.map
